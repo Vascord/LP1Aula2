7e7fed1ea71b66b5ee7939c3998b5e68e9621ee5
Vascord <duarte-vasco@outlook.com>
2020-03-05 12:51:19 +0000

    Ex9 done, saving everything

---
+++ Piramide/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Piramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give a number to build the piramide:");
+            string number = Console.ReadLine();
+            int number_int = Convert.ToInt32(number);
+
+            for(int i = number_int - 1, f = 1; i >= 0; i -= 1, f +=2)
+            {
+                for(int e = i ; e > 0 ; e-= 1)
+                {
+                    Console.Write(" ");
+                }
+                for(int u = f; u > 0; u -= 1)
+                {
+                    Console.Write("*");
+                }
+                for(int e = i; e > 0; e -= 1)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
