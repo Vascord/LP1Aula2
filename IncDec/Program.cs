4d12ab3585affef14906695f876b4a5b81b15995
Vascord <duarte-vasco@outlook.com>
2020-03-05 09:31:17 +0000

    Did the first ex1

---
+++ IncDec/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number_1 = 50;
+            int number_2;
+            number_2 = number_1++;
+            Console.WriteLine(number_2);
+            number_2 = --number_1;
+            Console.WriteLine(number_2);
+        }
+    }
+}
